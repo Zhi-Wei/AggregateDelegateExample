39d9d888aa51cf854b52728caa7f641c8ae2b177
Zhi-Wei <ZhiWei.Lu@outlook.com>
2017-08-18 19:32:10 +0800

    新建 FilterType 過濾器類型列舉。

---
+++ AggregateDelegateExample/Enums/FilterType.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace AggregateDelegateExample.Enums
+{
+    /// <summary>
+    /// 過濾器類型。
+    /// </summary>
+    [Flags]
+    public enum FilterType
+    {
+        /// <summary>
+        /// 無。
+        /// </summary>
+        None = 0,
+
+        /// <summary>
+        /// 奇數。
+        /// </summary>
+        Odd = 1,
+
+        /// <summary>
+        /// 偶數。
+        /// </summary>
+        Even = 2,
+
+        /// <summary>
+        /// 大於十。
+        /// </summary>
+        GreaterThanTen = 4,
+
+        /// <summary>
+        /// 小於十。
+        /// </summary>
+        LessThanTen = 8,
+
+        /// <summary>
+        /// 全部。
+        /// </summary>
+        All = Odd | Even | GreaterThanTen | LessThanTen
+    }
+}

4aa0c02631bf83370fcbcb7329378bc8ab62c876
Zhi-Wei <ZhiWei.Lu@outlook.com>
2017-08-21 07:12:17 +0800

    在 Program.Main 方法中新增範例類別的使用範例。

--- AggregateDelegateExample/Program.cs
+++ AggregateDelegateExample/Program.cs
@@ -1,15 +1,57 @@
 ﻿using AggregateDelegateExample.Enums;
+using AggregateDelegateExample.Services;
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using System.Linq.Expressions;
 
 namespace AggregateDelegateExample
 {
+    /// <summary>
+    /// 程式類別。
+    /// </summary>
     internal class Program
     {
+        /// <summary>
+        /// 主程式。
+        /// </summary>
+        /// <param name="args">參數集合。</param>
         private static void Main(string[] args)
         {
+            var filterType = FilterType.Odd | FilterType.GreaterThanTen;
+
+            var exampleServiceByCollection = new ExampleServiceByCollection();
+            var funcPredicateByCollection =
+                exampleServiceByCollection.GetFilterDelegatePredicate(_funcConditions, filterType);
+            var expressionPredicateByCollection =
+                exampleServiceByCollection.GetFilterExpressionPredicate(_expressionConditions, filterType);
+
+            var exampleServiceByDelegate = new ExampleServiceByDelegate();
+            var funcPredicateByDelegate =
+                exampleServiceByDelegate.GetFilterDelegatePredicate(_funcConditions, filterType);
+            var expressionPredicateByDelegate =
+                exampleServiceByDelegate.GetFilterExpressionPredicate(_expressionConditions, filterType);
+
+            var source = Enumerable.Range(1, 20);
+
+            Console.WriteLine("Func Filter Predicate By Collection:");
+            var funcResultByCollection = source.Where(funcPredicateByCollection);
+            PrintOut(funcResultByCollection);
+
+            Console.WriteLine("Func Filter Predicate By Delegate:");
+            var funcResultByDelegate = source.Where(funcPredicateByDelegate);
+            PrintOut(funcResultByDelegate);
+
+            Console.WriteLine("====================");
+
+            Console.WriteLine("Expression Filter Predicate By Collection:");
+            var expressionResultByCollection = source.Where(expressionPredicateByCollection.Compile());
+            PrintOut(expressionResultByCollection);
+
+            Console.WriteLine("Expression Filter Predicate By Delegate:");
+            var expressionResultByDelegate = source.Where(expressionPredicateByDelegate.Compile());
+            PrintOut(expressionResultByDelegate);
         }
 
         /// <summary>

6e9affa9780e56949f197f111e25d80ce713bb68
Zhi-Wei <ZhiWei.Lu@outlook.com>
2017-08-21 06:59:34 +0800

    新增 Program._funcConditions 委派篩選條件的私有靜態欄位。

--- AggregateDelegateExample/Program.cs
+++ AggregateDelegateExample/Program.cs
@@ -1,15 +1,45 @@
-﻿using System;
+﻿using AggregateDelegateExample.Enums;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace AggregateDelegateExample
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
         }
+
+        /// <summary>
+        /// 委派篩選條件。
+        /// </summary>
+        private static IEnumerable<KeyValuePair<Func<FilterType, bool>, Func<int, bool>>>
+            _funcConditions =
+            new List<KeyValuePair<Func<FilterType, bool>, Func<int, bool>>>(4)
+            {
+                new KeyValuePair<Func<FilterType, bool>, Func<int, bool>>
+                (
+                    filter => (filter & FilterType.Odd) == FilterType.Odd,
+                    number => number % 2 != 0
+                ),
+
+                new KeyValuePair<Func<FilterType, bool>, Func<int, bool>>
+                (
+                    filter => (filter & FilterType.Even) == FilterType.Even,
+                    number => number % 2 == 0
+                ),
+
+                new KeyValuePair<Func<FilterType, bool>, Func<int, bool>>
+                (
+                    filter => (filter & FilterType.GreaterThanTen) == FilterType.GreaterThanTen,
+                    number => number > 10
+                ),
+
+                new KeyValuePair<Func<FilterType, bool>, Func<int, bool>>
+                (
+                    filter => (filter & FilterType.LessThanTen) == FilterType.LessThanTen,
+                    number => number < 10
+                )
+            };
     }
 }

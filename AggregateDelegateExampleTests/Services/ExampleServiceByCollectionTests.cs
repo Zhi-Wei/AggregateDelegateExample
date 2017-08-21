5a59b33ebfd5289793b3c40a34a1c4dea24ff9d4
Zhi-Wei <ZhiWei.Lu@outlook.com>
2017-08-21 19:32:55 +0800

    1. 新建 AggregateDelegateExampleTests 測試專案。 2. 新建 ExampleServiceByCollectionTests 依照集合實作的範例服務測試類別。

---
+++ AggregateDelegateExampleTests/Services/ExampleServiceByCollectionTests.cs
@@ -0,0 +1,99 @@
+﻿using AggregateDelegateExample.Enums;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace AggregateDelegateExample.Services.Tests
+{
+    /// <summary>
+    /// 依照集合實作的範例服務測試類別。
+    /// </summary>
+    [TestClass()]
+    public class ExampleServiceByCollectionTests
+    {
+        #region -- 前置準備 --
+
+        /// <summary>
+        /// 來源資料。
+        /// </summary>
+        private IReadOnlyCollection<int> _sourceData =
+            Enumerable.Range(1, 20).ToList().AsReadOnly();
+
+        /// <summary>
+        /// 委派篩選條件。
+        /// </summary>
+        private static IEnumerable<KeyValuePair<Func<FilterType, bool>, Func<int, bool>>>
+            _funcConditions =
+            new List<KeyValuePair<Func<FilterType, bool>, Func<int, bool>>>(4)
+            {
+                new KeyValuePair<Func<FilterType, bool>, Func<int, bool>>
+                (
+                    filter => (filter & FilterType.Odd) == FilterType.Odd,
+                    number => number % 2 != 0
+                ),
+
+                new KeyValuePair<Func<FilterType, bool>, Func<int, bool>>
+                (
+                    filter => (filter & FilterType.Even) == FilterType.Even,
+                    number => number % 2 == 0
+                ),
+
+                new KeyValuePair<Func<FilterType, bool>, Func<int, bool>>
+                (
+                    filter => (filter & FilterType.GreaterThanTen) == FilterType.GreaterThanTen,
+                    number => number > 10
+                ),
+
+                new KeyValuePair<Func<FilterType, bool>, Func<int, bool>>
+                (
+                    filter => (filter & FilterType.LessThanTen) == FilterType.LessThanTen,
+                    number => number < 10
+                )
+            };
+
+        /// <summary>
+        /// 運算式樹狀架構篩選條件。
+        /// </summary>
+        private static IEnumerable<KeyValuePair<Func<FilterType, bool>, Expression<Func<int, bool>>>>
+            _expressionConditions =
+            new List<KeyValuePair<Func<FilterType, bool>, Expression<Func<int, bool>>>>(4)
+            {
+                new KeyValuePair<Func<FilterType, bool>, Expression<Func<int, bool>>>
+                (
+                    filter => (filter & FilterType.Odd) == FilterType.Odd,
+                    number => number % 2 != 0
+                ),
+
+                new KeyValuePair<Func<FilterType, bool>, Expression<Func<int, bool>>>
+                (
+                    filter => (filter & FilterType.Even) == FilterType.Even,
+                    number => number % 2 == 0
+                ),
+
+                new KeyValuePair<Func<FilterType, bool>, Expression<Func<int, bool>>>
+                (
+                    filter => (filter & FilterType.GreaterThanTen) == FilterType.GreaterThanTen,
+                    number => number > 10
+                ),
+
+                new KeyValuePair<Func<FilterType, bool>, Expression<Func<int, bool>>>
+                (
+                    filter => (filter & FilterType.LessThanTen) == FilterType.LessThanTen,
+                    number => number < 10
+                )
+            };
+
+        /// <summary>
+        /// 取得系統測試實體。
+        /// </summary>
+        /// <returns></returns>
+        private ExampleServiceByCollection GetSystemUnderTestInstance()
+        {
+            return new ExampleServiceByCollection();
+        }
+
+        #endregion -- 前置準備 --
+    }
+}
